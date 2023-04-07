bce51c413e2569e1f95d94d1a7f4d5380ffa6ecf
Sup3r-Us3r <maydersonmello@gmail.com>
2023-04-07 09:52:54 -0300

    create animation on progress bar component

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,6 +1,8 @@
 import { View } from 'react-native';
 
 import { styles } from './styles';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import { useEffect } from 'react';
 
 interface Props {
   total: number;
@@ -10,9 +12,19 @@ interface Props {
 export function ProgressBar({ total, current }: Props) {
   const percentage = Math.round((current / total) * 100);
 
+  const sharedProgress = useSharedValue(percentage);
+
+  const styleAnimated = useAnimatedStyle(() => ({
+    width: `${sharedProgress.value}%`
+  }));
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(percentage);
+  }, [current]);
+
   return (
     <View style={styles.track}>
-      <View style={[styles.progress, { width: `${percentage}%` }]} />
+      <Animated.View style={[styles.progress, styleAnimated]} />
     </View>
   );
 }
--- src/components/ProgressBar/styles.ts
+++ src/components/ProgressBar/styles.ts
@@ -1,15 +1,16 @@
 import { StyleSheet } from 'react-native';
+import { THEME } from '../../styles/theme';
 
 export const styles = StyleSheet.create({
   track: {
     height: 8,
     width: '100%',
     borderRadius: 8,
-    backgroundColor: '#505059'
+    backgroundColor: THEME.COLORS.GREY_500
   },
   progress: {
     height: 8,
-    backgroundColor: '#00B37E',
+    backgroundColor: THEME.COLORS.BRAND_LIGHT,
     borderRadius: 8,
   }
 });
