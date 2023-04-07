ae8db6075b365ee65d03126855d2073c3b1e653e
Sup3r-Us3r <maydersonmello@gmail.com>
2023-04-07 19:35:18 -0300

    add vibration as feedback when user gives wrong answer

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -14,6 +14,7 @@ import Animated, {
 import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Audio } from 'expo-av';
+import * as Haptics from 'expo-haptics';
 
 import { styles } from './styles';
 import { THEME } from '../../styles/theme';
@@ -134,7 +135,9 @@ export function Quiz() {
     return true;
   }
 
-  function shakeAnimation() {
+  async function shakeAnimation() {
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce }),
       withTiming(0, undefined, finished => {
