b7727e7ce18a5cbe1d356f94c75cb4136238e2a0
Sup3r-Us3r <maydersonmello@gmail.com>
2023-04-07 19:23:47 -0300

    play audio file as feedback for user

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -12,8 +12,8 @@ import Animated, {
   runOnJS,
 } from 'react-native-reanimated';
 import { GestureDetector, Gesture } from 'react-native-gesture-handler';
-
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { Audio } from 'expo-av';
 
 import { styles } from './styles';
 import { THEME } from '../../styles/theme';
@@ -55,6 +55,17 @@ export function Quiz() {
   const route = useRoute();
   const { id } = route.params as Params;
 
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require('../../assets/correct.mp3')
+      : require('../../assets/wrong.mp3');
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true });
+
+    await sound.setPositionAsync(0);
+    await sound.playAsync();
+  }
+
   function handleSkipConfirm() {
     Alert.alert('Pular', 'Deseja realmente pular a questão?', [
       { text: 'Sim', onPress: () => handleNextQuestion() },
@@ -91,11 +102,15 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
-      setStatusReply(1);
       setPoints(prevState => prevState + 1);
 
+      await playSound(true);
+
+      setStatusReply(1);
       handleNextQuestion();
     } else {
+      await playSound(false);
+
       setStatusReply(2);
       shakeAnimation();
     }
