98737a309d0c994ab35ae44811f259def078cfed
Sup3r-Us3r <maydersonmello@gmail.com>
2023-04-07 11:36:19 -0300

    install react native gesture handler

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -14,13 +17,14 @@ export default function App() {
   }
 
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
+
       <Routes />
-    </>
+    </GestureHandlerRootView>
   );
 }
